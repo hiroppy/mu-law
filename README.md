f3419dfaa04bf5171aec6f2ccbaa5e8225f6122d
abouthiroppy <git@about-hiroppy.com>
2016-12-19 13:50:48 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# mu-law
+
+## Install
+```
+$ npm install mu-law
+```
+
+## Usage
+```javascript
+const muLaw = require('mu-law');
+
+muLaw.encode();
+muLaw.decode();
+```
---
+++ index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+// http://dystopiancode.blogspot.jp/2012/02/pcm-law-and-u-law-companding-algorithms.html
+
+module.exports.encode = require('./lib/encode');
+module.exports.decode = require('./lib/decode');
